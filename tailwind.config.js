61b7b3d1181748e2f0d019bda557ea96291797e0
gracebir <grace.devolop@gmail.com>
2023-05-25 00:44:24 +0200

    setting up tailwindcss

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'josefin-sans':['Josefin Sans', 'sans-serif']
+      }
+    },
   },
   plugins: [],
 }
