0249722f68b55142f8431ae524ed0e48e9ae67c3
gracebir <grace.devolop@gmail.com>
2023-05-25 08:56:22 +0200

    install react router dom packages

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from 'react'
 
+
+/**
+ * background image
+ * desktop lightmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-light_j9xyeb.jpg
+ * desktop darkmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-dark_crnpoj.jpg
+ * mobile darkmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-dark_pi6pt4.jpg
+ * mobile lightmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-light_ky85gd.jpg
+*/
+
+
 function App() {
   return (
     <div className="font-josefin-sans">
