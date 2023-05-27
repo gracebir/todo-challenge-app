8f61647849ea9ec32bebdf6dc6cf5a5f0b1745bb
gracebir <grace.devolop@gmail.com>
2023-05-27 12:53:30 +0200

    config theme switching

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import Moon from './assets/icon-moon.svg'
 import Sun from './assets/icon-sun.svg'
-
-/**
- * background image
- * desktop lightmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-light_j9xyeb.jpg
- * desktop darkmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-dark_crnpoj.jpg
- * mobile darkmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-dark_pi6pt4.jpg
- * mobile lightmode: https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-light_ky85gd.jpg
-*/
-
+import TodoForm from './components/TodoForm'
 
 function App() {
   const [theme, setTheme] = useState("dark")
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add("dark")
+    } else {
+      document.documentElement.classList.remove("dark")
+    }
+  }, [theme])
+
   const toggle = () => {
-    if(theme === "dark"){
+    if (theme === "dark") {
       setTheme("light")
-    } else if(theme==="light"){
+    } else if (theme === "light") {
       setTheme("dark")
     }
   }
+
   return (
-    <div className="font-josefin-sans bg-image-desktop bg-no-repeat bg-cover h-[35vh] w-full">
-      <div className='max-w-2xl h-screen top-50 mx-auto  flex flex-col items-center justify-center'>
-        <div className='w-full h-[80vh]'>
-          <div className="flex justify-between items-center">
-            <h1 className="text-white text-4xl uppercase font-bold tracking-[.2em]">todo</h1>
-            <img onClick={toggle} className="cursor-pointer" src={theme==="dark" ? Sun: Moon } alt="" />
+    <div className="bg-bg-light dark:bg-bg-dark h-screen">
+      <div className="font-josefin-sans lg:bg-desk-bg-light bg-mob-bg-light dark:bg-mob-bg-dark lg:dark:bg-desk-bg-dark bg-no-repeat bg-cover h-[35vh] w-full px-4 lg:px-0">
+        <div className='max-w-2xl h-screen top-50 mx-auto  flex flex-col items-center justify-center'>
+          <div className='w-full h-[80vh] flex flex-col gap-12'>
+            <div className="flex justify-between items-center">
+              <h1 className="text-white text-4xl uppercase font-bold tracking-[.2em]">todo</h1>
+              <img onClick={toggle} className="cursor-pointer" src={theme === "dark" ? Sun : Moon} alt="" />
+            </div>
+            <div className='flex flex-col gap-6'>
+              <TodoForm/>
+              <div>
+
+              </div>
+            </div>
           </div>
+          <p className='text-text-footer-dark'>Drag and Drop to recorder list</p>
         </div>
       </div>
     </div>
--- tailwind.config.js
+++ tailwind.config.js
@@ -4,10 +4,31 @@ export default {
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode:'class',
   theme: {
     extend: {
       fontFamily: {
         'josefin-sans':['Josefin Sans', 'sans-serif']
+      },
+      backgroundImage: {
+        // light bg
+        "desk-bg-light":"url('https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-light_j9xyeb.jpg')",
+        "mob-bg-light": "url('https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-light_ky85gd.jpg')",
+        // dark bg
+        "desk-bg-dark":"url('https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-desktop-dark_crnpoj.jpg')",
+        "mob-bg-dark": "url('https://res.cloudinary.com/dnzidlufh/image/upload/v1684996733/bg-mobile-dark_pi6pt4.jpg')",
+      },
+      colors:{
+        "bg-dark": "var(--very-dark-blue)",
+        "bg-light":"var(--color-very-light-gray)",
+        "text-todos-dark": "var(--color-very-light-gray)",
+        "text-todos-light": "var(--very-dark-blue)",
+        "text-footer-dark": "var(--very-dark-grayish-blue)",
+        "text-footer-dark-hover":"var(--light-grayish-blue)",
+        "bg-input-dark": "var(--dark-desaturated-blue)",
+        "bg-input-light":"var(--color-very-light-grayish-blue)",
+        "gray-text-dark": "var(--very-dark-grayish-blue)",
+        "gray-text-light": "var(--dark-grayish-blue)"
       }
     },
   },
