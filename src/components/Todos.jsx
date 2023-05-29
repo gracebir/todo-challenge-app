0cf384e612ebf31da964031feebf47d85e1d731e
gracebir <grace.devolop@gmail.com>
2023-05-29 18:33:01 +0200

    todo components

--- src/components/Todos.jsx
+++ src/components/Todos.jsx
@@ -1,11 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
+import CheckBox from './CheckBox'
+import cross from '../assets/icon-cross.svg'
 
-function Todos({isCompleted, text, id}) {
+function Todo({text, id}) {
+  const [isCompleted, setIsCompleted] = useState(false)
+  console.log(isCompleted)
   return (
-    <div>
-      
+    <div className="flex px-8 py-4 items-center justify-between">
+      <div className="flex items-center gap-4">
+        <CheckBox isCompleted={isCompleted} setIsCompleted={setIsCompleted}/>
+        <p className={`text-2xl ${isCompleted ? "line-through text-text-footer-light dark:text-text-footer-dark":"text-text-todos-light dark:text-primary-dark"}`}>{text}</p>
+      </div>
+      <img src={cross} alt="cross" />
     </div>
   )
 }
 
-export default Todos
+export default Todo
